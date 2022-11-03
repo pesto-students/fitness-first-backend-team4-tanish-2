9682c4420dde021faf4639c47eadbac865a7af46
Chinmaay-Patil <102859886+Chinmaay-Patil@users.noreply.github.com>
2022-11-03 19:36:07 +0530

    backend deployment

---
+++ constants.js
@@ -0,0 +1,15 @@
+const Constants = {
+    ROLE_TRAINER : "trainer",
+    ROLE_CLIENT : "client"
+}
+
+console.log(JSON.parse('{"name":"pratik","age":26,"address":"gadge nagar near radhakrishna temple amravati.."}' ));
+
+module.exports = Constants;
+
+
+
+let obj =  {"date": "2022-08-27T06:12:29.097Z", 
+"name": "pratik lavhale", 
+"likes": 0, "image_path": "", 
+"post_description": "testing"}
---
+++ storedProcedures.js
@@ -0,0 +1,8 @@
+const Stored_Procedures = {
+    TRAINER_REGISTRATION : "Trainer_Registration",
+    GET_TRAINER_DETAILS : "Get_Trainer_Details",
+    NEW_POST : "New_Post"
+}
+
+module.exports = Stored_Procedures;
+
